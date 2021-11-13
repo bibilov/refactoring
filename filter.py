82d0a8cddc5f3745b4374f62d4c9b3c48d6b73d2
udavspokoinyi <60442562+udavspokoinyi@users.noreply.github.com>
2021-11-13 16:49:31 +0500

    Update filter.py
    !_PEP8.
    !_Именование переменных.
    !_Возможность управлять размерами мозайки (сейчас — только 10x10).
    !_Возможность управлять градациями серого (сейчас — с шагом 50). Лучше сделать просто в виде задания количества шагов. Например: 4 градации, 6 градаций.
    !_Выделение функций.

--- filter.py
+++ filter.py
@@ -2,28 +2,53 @@ from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+m_w = len(arr)
+m_h = len(arr[1])
 i = 0
+step = None
+#ПЕРЕВОД В СТИЛЬ PEP8
 
-while i < a : #Неверно работаю с граничными условиями, в результате чего справа и внизу остались необработанные полосы по 10 пикселей.
+
+#ыделение функций
+
+
+def find_step(n):
+    return int(255 / (n - 1) - 1 if 255 % (n - 1) == 0 else 255 / (n - 1)) #градации серого
+
+
+#выделение функций 
+def find_av_brightness(i, j, arr, m_h, m_w, step):
+   while i < a :
     j = 0
-    while j < a1 : #Неверно работаю с граничными условиями, в результате чего справа и внизу остались необработанные полосы по 10 пикселей.
+    while j < a1 :
         s = 0
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                n0 = arr[n][n1][0] #В одном месте я запутался с именами переменных.
+                n0 = arr[n][n1][0]
                 n2 = arr[n][n1][1]
                 n3 = arr[n][n1][2]
-                M = n0 + n2 + n3 #В одном месте я запутался с именами переменных.
-                s += M / 3 #Неверно считаю компоненты серого цвета (забываю поделить на 3).
+                M = n0 + n2 + n3
+                s += M/3
         s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+    return s
+#выделение функций
+
+
+def do_mosaic(i, j, arr, m_h, m_w, step):#мозайка на ручном управление
+    while i < a :
+        j = 0
+            while j < a1 :
+                for n in range(i, i + 10):
+                    for n1 in range(j, j + 10):
+                        arr[n][n1][0] = int(s // 50) * 50
+                        arr[n][n1][1] = int(s // 50) * 50
+                        arr[n][n1][2] = int(s // 50) * 50
+                j = j + 10
+            i = i + 10
+    return i
+
+
+inp_sizes = input('Введите ширину и высоту мозайки (например: 100,100): ')#мозайка на ручном управление
+num_grad = int(input('Enter the number of gradations(for example: 6): '))#шаги серого ручное управление
 res = Image.fromarray(arr)
 res.save('res.jpg')
