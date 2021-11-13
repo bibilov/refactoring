70aa9de77e8e758f8501b8d908922e5d928b591f
udavspokoinyi <60442562+udavspokoinyi@users.noreply.github.com>
2021-11-13 16:56:39 +0500

    Update filter.py
    !_Возможно, переписать в консольную утилиту, которой на вход подаются имена исходного изображения и результата. Сейчас чтобы заставить код работать с другой картинкой, его надо исправлять.

--- filter.py
+++ filter.py
@@ -1,12 +1,10 @@
 from PIL import Image
-import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-
+#переолнение numpy
+# ручной ввод изображения!
 
 def find_step(n):
     return int(255 / (n - 1) - 1 if 255 % (n - 1) == 0 else 255 / (n - 1))
-#Преобразование в матричный вид
+
 
 def find_av_brightness(i, j, arr, m_h, m_w, step):
     br = 0.
@@ -14,7 +12,7 @@ def find_av_brightness(i, j, arr, m_h, m_w, step):
     br //= m_h * m_w
     br -= br % step
     return br
-#Преобразование в матричный вид
+
 
 def do_mosaic(arr, m_h, m_w, step):
     for i in range(0, len(arr), m_h):
@@ -23,10 +21,22 @@ def do_mosaic(arr, m_h, m_w, step):
             arr[i: i + m_h, j: j + m_w, :] = brightness
 
 
+inp_im_names = input('Введите название изображение '
+                     'Ввод выглядит как (например: img2,res): ') \
+    .split(',')
+
 
 inp_sizes = input('Введите ширину и высоту мозайки (например: 100,100): ')#мозайка на ручном управление
+
+
 num_grad = int(input('Enter the number of gradations(for example: 6): '))#шаги серого ручное управление
+#градация серого и мазайка ручное управление через матрицу
+
+
 e_h, e_w = map(int, inp_sizes.split(','))
-do_mosaic(inp_im, e_h, e_w, find_step(num_grad)) #градация серого и мазайка ручное управление через матрицу
-res = Image.fromarray(arr)
-res.save('res.jpg')
+do_mosaic(inp_im, e_h, e_w, find_step(num_grad))
+
+res = Image.fromarray(inp_im)
+
+
+res.save(inp_im_names[1] + ".jpg")
